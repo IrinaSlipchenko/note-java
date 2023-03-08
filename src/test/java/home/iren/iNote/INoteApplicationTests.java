3af8b73cae6205dcf8ed6fb99984b028afb52eb1
IrinaSlipchenko <slpchnk.irina@yandex.ru>
2023-03-08 20:26:49 +0300

    init

---
+++ src/test/java/home/iren/iNote/INoteApplicationTests.java
@@ -0,0 +1,13 @@
+package home.iren.iNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class INoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d4ed5d2ab9f0a71b65785b3fcb732d3af5d59d62
IrinaSlipchenko <slpchnk.irina@yandex.ru>
2023-03-09 21:51:11 +0300

    fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO notes (note_id, note_content)
-    VALUES (default, 'Создание одной простейшей заметки только с текстом');
+VALUES (default, 'Создание одной простейшей заметки только с текстом');
 INSERT INTO notes (note_id, note_content)
-    VALUES (default, 'Редактирование заметки');
+VALUES (default, 'Редактирование заметки');
 INSERT INTO notes (note_id, note_content)
-    VALUES (default, 'Выводить список существующих заметок');
+VALUES (default, 'Выводить список существующих заметок');
